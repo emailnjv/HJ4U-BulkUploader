7a4801f49c9d195d22dbb6692690a3dddc34164b
Nick Vincent <emailnjv@gmail.com>
2020-05-14 23:37:12 -0400

    Category Service

--- cmd/category_service/main.go
+++ cmd/category_service/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
+
+	"github.com/emailnjv/HJ4U-BulkUploader/siteClient"
+	"github.com/emailnjv/HJ4U-BulkUploader/utils"
 )
 
 func init() {
 }
 
 func main() {
-	exit(run())
+	err := run("Art Prints", 11, 25)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func exit(err error) {
@@ -19,10 +26,85 @@ func exit(err error) {
 	os.Exit(1)
 }
 
-func run() error {
+func run(targetCategory string, catID int, subCatID int) error {
+	var csvLineArr []utils.CSVLine
+
+	lines, err := ReadCsv("/home/nick/Documents/Projects/Work/Dad/HotJewelry4U/BulkUploader/resources/listings.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	sc, err := siteClient.NewSiteClient()
+	if err != nil {
+		return err
+	}
+	defer sc.CloseSiteClient()
+
+	// Loop through lines & turn into object
+	for index, line := range lines {
+		if index != 0 {
+			data := utils.CSVLine{
+				ItemID:             line[0],
+				CustomLabel:        line[1],
+				ProductIDType:      line[2],
+				ProductIDValue:     line[3],
+				ProductIDValue2:    line[4],
+				QuantityAvailable:  line[5],
+				Purchases:          line[6],
+				Bids:               line[7],
+				Price:              line[8],
+				StartDate:          line[9],
+				EndDate:            line[10],
+				Condition:          line[11],
+				Type:               line[12],
+				ItemTitle:          line[13],
+				CategoryLeafName:   line[14],
+				CategoryNumber:     line[15],
+				PrivateNotes:       line[16],
+				SiteListed:         line[17],
+				DownloadDate:       line[18],
+				VariationDetails:   line[19],
+				ProductReferenceID: line[20],
+				ConditionID:        line[21],
+				OutOfStockControl:  line[22],
+			}
+			if data.CategoryLeafName == targetCategory {
+				csvLineArr = append(csvLineArr, data)
+			}
+
+
+		}
+	}
+
+	for _, csvLine := range csvLineArr {
+		printUsage(csvLine.ItemID)
+		err = sc.InsertListing(catID, subCatID, csvLine)
+		if err != nil {
+			return fmt.Errorf("error inserting item %v; err = %v", csvLine.ItemID, err)
+		}
+	}
+
+	return nil
 }
 
-func printUsage() {
-	fmt.Println("")
-	os.Exit(0)
+func ReadCsv(filename string) ([][]string, error) {
+
+	// Open CSV file
+	f, err := os.Open(filename)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+
+	// Read File into a Variable
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return lines, nil
+}
+
+func printUsage(itemID string) {
+	fmt.Println("Inserting Product ID %v", itemID)
 }

6a21093abbbc460cf07a1cbef3825bd3ec04d201
Nick Vincent <emailnjv@gmail.com>
2020-05-14 23:35:28 -0400

    GetProductInfo

--- ebay/ebay.go
+++ ebay/ebay.go
@@ -4,6 +4,9 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+
+	"github.com/emailnjv/HJ4U-BulkUploader/db"
+	"github.com/emailnjv/HJ4U-BulkUploader/utils"
 )
 
 type EbayClient struct {
@@ -25,4 +28,11 @@ func NewEbayClient() (EbayClient, error) {
 	return result, err
 }
 
+func (ec *EbayClient) GetProductInfo(catID int, subCatID int, csvData utils.CSVLine) (db.Product, []string, error) {
+	apiResponse, err := ec.getItem(csvData.ItemID)
+	if err != nil {
+		return db.Product{}, []string{}, err
+	}
+	return ec.parseItem(catID, subCatID, csvData, apiResponse)
+}
 
